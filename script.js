ca4dc7a3660fa14c3b35f0deb50a102d80d19f7e
kappyllo <33810546+kappyllo@users.noreply.github.com>
2022-10-23 17:38:42 +0200

    s

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+//selecting dom's
+
+const busTime = document.querySelector(".bus--time--input");
+const sixHours = document.querySelector(".six--hours");
+const sevenHours = document.querySelector(".seven--hours");
+const eightHours = document.querySelector(".eight--hours");
+const nineHours = document.querySelector(".nine--hours");
+const submit = document.querySelector(".give--me--answer--input");
+
+let busHour;
+let selectedAmountOfHours;
+let isChoosen = false;
+
+submit.addEventListener("click", function () {
+  if (isChoosen == false) {
+    submit.onclick = "location.href='d.html';";
+  }
+});
