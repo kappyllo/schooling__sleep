1fb5fb31686540fc964f8af3e205dd5d68020011
kappyllo <33810546+kappyllo@users.noreply.github.com>
2022-10-23 18:32:37 +0200

    finally

--- script.js
+++ script.js
@@ -9,15 +9,19 @@ const eightHours = document.querySelector(".eight--hours");
 const nineHours = document.querySelector(".nine--hours");
 const submit = document.querySelector(".give--me--answer--input");
 
+//adding variables
 let busHour;
 let selectedAmountOfHours;
 let isChoosenHour = false;
 let isChoosenBus = false;
 
+//functions
 const selectingHours = function () {
   isChoosenHour = true;
   return isChoosenHour;
 };
+
+//event listeners
 
 sixHours.addEventListener("click", function () {
   selectingHours();
@@ -36,6 +40,9 @@ nineHours.addEventListener("click", function () {
 });
 
 submit.addEventListener("click", function () {
+  if (busTime.value !== "") {
+    isChoosenBus = true;
+  }
   if ((isChoosenHour === true) & (isChoosenBus === true)) {
     window.location = "answer.html";
   }
